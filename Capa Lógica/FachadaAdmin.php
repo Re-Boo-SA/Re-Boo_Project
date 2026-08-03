d5cf4f72be3b0f2426c8c1911afbb6aecb706211
TestUser1d <hojasrayadas100@gmail.com>
2026-08-02 21:12:07 -0300

    Lastt

--- Capa Lógica/FachadaAdmin.php
+++ Capa Lógica/FachadaAdmin.php
@@ -1,2 +1,12 @@
 <?php
+
+require_once(__DIR__ . '/../DTO/AdministradorDTO.php');
+require_once('/IAdminLogica.php');
+
+class Fachada {
+
+    public function retornoIAdminLogica(): IAdminLogica{
+        $unIAL =
+    }
+}
 ?>
